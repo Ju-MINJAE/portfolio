222ceb22d0ef69b1175bd2d2f0c3433196b360d5
Ju-MINJAE <juminjae98@gmail.com>
2024-12-13 13:24:26 +0900

    design: apply custom color from tailwind config

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,14 +4,16 @@ import React, { useRef } from 'react';
 import Image from 'next/image';
 import { motion, useInView } from 'framer-motion';
 import { ProjectText } from '@/constants/layout';
-import { ExternalLink, Github } from 'lucide-react';
+import { ExternalLink, Github, Calendar } from 'lucide-react';
 
 interface ProjectData {
   title: string;
   desc: string;
+  features: string[];
+  duration: string;
   stack: string[];
   image?: string;
-  liveLink?: string;
+  liveLink?: string | null;
   githubLink?: string;
 }
 
@@ -48,7 +50,7 @@ const ProjectItem = ({
   return (
     <motion.div
       ref={ref}
-      className={`flex flex-col md:flex-row  mb-20 ${
+      className={`flex flex-col md:flex-row mb-20 ${
         index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'
       }`}
       initial={{ opacity: 0, y: 50 }}
@@ -61,7 +63,7 @@ const ProjectItem = ({
             <Image
               src={project.image}
               alt={project.title}
-              sizes="80vw, 24vw"
+              sizes="(max-width: 768px) 100vw, 50vw"
               fill
               style={{ objectFit: 'cover' }}
               className="rounded-lg shadow-lg"
@@ -77,16 +79,35 @@ const ProjectItem = ({
         <h3 className="text-2xl font-semibold mb-4 text-white">
           {project.title}
         </h3>
+        <div className="flex items-center mb-2 text-gray-400">
+          <Calendar className="mr-2" size={20} />
+          <span>{project.duration}</span>
+        </div>
         <p className="text-gray-400 mb-6">{project.desc}</p>
         <div className="mb-6">
-          {project.stack.map((tech, techIndex) => (
-            <span
-              key={techIndex}
-              className="inline-block bg-blue-600 text-white rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2"
-            >
-              {tech}
-            </span>
-          ))}
+          <h4 className="text-lg font-semibold text-white mb-2 flex items-center">
+            주요 기능
+          </h4>
+          <ul className="list-disc list-inside text-gray-400">
+            {project.features.map((feature, featureIndex) => (
+              <li key={featureIndex}>{feature}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="mb-6">
+          {project.stack.map((tech, techIndex) => {
+            const colorClass = `bg-${tech.toLowerCase()}`;
+
+            return (
+              <span
+                key={techIndex}
+                className={`inline-block ${colorClass} text-white rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2`}
+              >
+                {tech}
+              </span>
+            );
+          })}
         </div>
         <div className="flex items-center space-x-4">
           {project.liveLink && (
--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -80,24 +80,45 @@ export const skillText = [
 export const ProjectText = [
   {
     title: 'Villez',
-    desc: '개인 물품 대여 C2C 서비스',
+    desc: '개인 간의 물품 대여와 반납을 지원하는 C2C 서비스입니다.',
+    features: [
+      '사용자 로그인 및 인증',
+      '물품 등록 및 예약 기능',
+      '실시간 채팅 지원',
+    ],
+    duration: '2023.10 ~ 2023.12',
     stack: ['React', 'Firebase', 'PWA'],
     image: '/image/test.jpeg',
     githubLink: 'https://github.com/Ju-MINJAE/Viliez/tree/main/viliez-pwa',
+    liveLink: null,
   },
   {
     title: '그리핀도미토리',
-    desc: '대학교 기숙사 룸메이트',
+    desc: '기숙사 룸메이트를 구할 수 있는 플랫폼입니다.',
+    features: [
+      '자유게시판: 글 작성 및 댓글 기능',
+      '룸메이트 모집 게시판: 모집글 작성 및 신청 기능',
+      '사용자 관리 및 인증',
+    ],
+    duration: '2024.04 ~ 2024.06',
     stack: ['Vue', 'Firebase', 'PWA'],
     image: '/image/Griffindormitory.png',
     githubLink: 'https://github.com/Ju-MINJAE/Griffindormitory',
+    liveLink: null,
   },
   {
     title: '커뮤니T',
-    desc: '무한 CRUD 반복',
-    stack: ['React', 'TypeScript', 'Axios', 'Vercel', 'TailwindCSS'],
+    desc: '운동 애호가를 위한 커뮤니티 플랫폼입니다.',
+    features: [
+      '자유게시판: 사용자 간 소통 및 정보 공유',
+      '챌린지 게시판: 챌린지 생성 및 참여 기능',
+      '이벤트 관리: 공동 목표 설정 및 진행',
+    ],
+    duration: '2024.10 ~ 2024.11',
+    stack: ['React', 'TypeScript', 'TailwindCSS', 'Axios', 'Vercel'],
     image: '/image/123.png',
     githubLink: 'https://github.com/Ju-MINJAE/communiT',
+    liveLink: 'https://advanced-class-project.vercel.app/',
   },
 ];
 
