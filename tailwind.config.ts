4ecd5a03cecd8d96172981a490725969da027682
Ju-MINJAE <juminjae98@gmail.com>
2024-12-13 13:17:05 +0900

    design: add custom colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        react: '#61DAFB',
+        vue: '#4FC08D',
+        typescript: '#3178C6',
+        axios: '#5A29E4',
+        firebase: '#DD2C00',
+        pwa: '#5A0FC8',
+        vercel: '#000000',
+        tailwindcss: '#06B6D4',
       },
     },
   },
   plugins: [],
+  safelist: [
+    'bg-react',
+    'bg-vue',
+    'bg-typescript',
+    'bg-axios',
+    'bg-firebase',
+    'bg-pwa',
+    'bg-vercel',
+    'bg-tailwindcss',
+  ],
 } satisfies Config;
