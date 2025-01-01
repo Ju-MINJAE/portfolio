45f9f80c702addf69b6ee8b2015b864885a4f67c
Ju-MINJAE <juminjae98@gmail.com>
2025-01-01 22:00:20 +0900

    [refactor] modify image size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -27,43 +27,52 @@ const About = () => {
   };
 
   return (
-    <section id="About" className="bg-gray-900 text-white py-20">
-      <div className="container mx-auto px-4">
+    <section className="bg-gray-900 text-white py-20">
+      <div className="container mx-auto px-4 max-w-5xl">
         <motion.div
-          className="flex flex-col md:flex-row items-center "
+          className="flex flex-col md:flex-row items-center justify-between"
           variants={containerVariants}
           initial="hidden"
           animate="visible"
         >
-          <motion.div className="md:w-1/2 mb-8 md:mb-0" variants={itemVariants}>
-            <Image
-              src={AboutData.img}
-              alt="MinJae's profile"
-              width={400}
-              height={400}
-              className="rounded-3xl shadow-lg object-cover "
-            />
+          <motion.div className="md:w-1/2" variants={itemVariants}>
+            <div className="relative w-full aspect-square md:pr-8">
+              <Image
+                src={AboutData.img}
+                alt="MinJae's profile"
+                width={300}
+                height={300}
+                className="rounded-3xl shadow-lg object-cover"
+              />
+            </div>
           </motion.div>
-          <motion.div className="md:w-1/2 md:pl-4" variants={containerVariants}>
-            <h2 className="text-4xl font-bold mb-6">About Me</h2>
+          <motion.div
+            className="md:w-1/2 md:pl-8 pt-8 md:pt-0"
+            variants={containerVariants}
+          >
+            <motion.h2
+              className="text-4xl font-bold mb-8 text-blue-400"
+              variants={itemVariants}
+            >
+              About Me
+            </motion.h2>
             {AboutData.desc.map((text, index) => (
               <motion.p
                 key={index}
-                className="text-xl mb-4"
+                className="text-lg mb-4 leading-relaxed text-gray-300"
                 variants={itemVariants}
               >
                 {text}
               </motion.p>
             ))}
-
-            <motion.div className="flex space-x-4" variants={itemVariants}>
+            <motion.div className="flex space-x-6 mt-6" variants={itemVariants}>
               <a
                 href="https://github.com/Ju-MINJAE"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-400 hover:text-blue-300 transition-colors"
               >
-                <Github size={25} />
+                <Github size={28} />
               </a>
               <a
                 href="https://velog.io/@min_jae/posts"
@@ -71,7 +80,7 @@ const About = () => {
                 rel="noopener noreferrer"
                 className="text-blue-400 hover:text-blue-300 transition-colors"
               >
-                <BookOpen size={25} />
+                <BookOpen size={28} />
               </a>
             </motion.div>
           </motion.div>
