555c1ff40f8f893a80bf92659fb9624f863b35cf
Ju-MINJAE <juminjae98@gmail.com>
2025-02-16 11:38:18 +0900

    chore: Add Custom Tailwind Colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        tech: {
+          react: '#6cd4f1',
+          typescript: '#3178C6',
+          javascript: '#ead434',
+          tailwind: '#06B6D4',
+          firebase: '#DD2C00',
+          redux: '#764ABC',
+          axios: '#5A29E4',
+        },
       },
     },
   },
